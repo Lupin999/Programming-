61fd01f9d937b9e2e8bbc88c7b2b32c9ef4ee432
lupin999 <joelsanya47@gmail.com>
2024-07-05 12:10:16 -0600

    first commit

---
+++ student details.c
@@ -0,0 +1,29 @@
+//A C programme displaying student details
+#include <stdio.h>
+#include <string.h>
+typedef struct;
+char firstname;
+char lastname;
+char email;
+int RegNomb;
+int DOB;
+int age;
+int phonenumber;
+student}
+int main(){
+strcpy(student.firstname."Joel");
+strcpy(student.lastname."Sanya");
+strcpy(student.email."mayubejoel@zetech.ac.ke");
+student.RegNom=14;
+student.DOB=27September2006;
+student.age=18;
+student.phonenumber=0111206771;
+printf("firstname:%s\n",Joel);
+printf("lastname:%s\n",Sanya);
+printf("email:%s\n",mayubejoel@zetech.ac.ke);
+printf("RegNom:%d\n",14);
+printf("DOB:%d\n",27September2006);
+printf("age:%d\n".17);
+printf("phonenumber:%d\n"0111206771);
+return 0;
+}
