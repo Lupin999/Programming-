28b54d6bd0eb54900e736db4910154e1ae62d496
Lupin999 <joelsanya47@gmail.com>
2024-07-11 11:15:05 +0300

    Update Salary programme.c

--- Salary programme.c
+++ Salary programme.c
@@ -10,10 +10,10 @@ float salary = 50.67; //%f
 short budget = 35.78; //%lf
 char a = 'c', //%c
 printf("Please enter the value of longlong;");
-scanf("%d,&longlong);
+scanf("%d",&longlong);
 printf("The value of longlong is %d\n",longlong);
-printf("The value of salary is %10.2f\n",salary);
-printf("The value of budget is %.2lf\n",budget);
-printf("The value of a is %c \n",a);
+printf("The value of salary is %.2f\n",salary);
+printf("The value of budget is %.2f\n",budget);
+printf("The value of a is %c\n",a);
 return 0;
 }
